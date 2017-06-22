5c555e7698e84992026f2b429f0a790c5eae8dda
Simon Zu <uznomis@users.noreply.github.com>
2017-06-22 11:50:30 -0500

    Simon work half way.

--- Strain_gauge_view.m
+++ Strain_gauge_view.m
@@ -2,23 +2,26 @@
 % Jeffers, display the data according to the user request
 %% Initializing
 % paramters to change before continuing
-cardSN = [1, 2];    % which cards to import; numbers should match file names
+cardSN = [1,2];    % which cards to import
 freq = 5e5;    % freqeuncy in Hz
-cardToGetEncoder = 1;    % the number of card whose encoder data is used for velocity and counter calculation
-encoderChannels = [16;17];
+cardToGetEncoder = 0;    % the number of card whose encoder data is used for velocity and counter calculation; put 0 if you don't want velocity and counter
+encoderChannels = [16;17];    % encoder channels for the two cards; should match the ordering in cardSN
 encoderAvailable = 1;    % 1 for yes, 0 for no; if no encoder is available, there is no sync between cards
 lenPerSector = 1.5e-6;    % encoder sector length in meters
 
 %% Importing
-for i = 1:2
+filename = cell(length(cardSN));
+filepath = cell(length(cardSN));
+for i = 1:length(length(cardSN))
     [filename{i}, filepath{i}] = uigetfile...
         ('C:\Users\User\Documents\Test files results on fast cards\*.*',...
-        ['Please select ACQ100', num2str(i)]);
+        ['Please select ACQ100', num2str(cardSN(i))]);
     if filename{i} == 0
         return
     end
 end
-for i = 1:2
+test = cell(length(cardSN));
+for i = 1:length(cardSN)
     test{i} = dlmread([filepath{i}, filename{i}]);
     test{i} = reshape(test{i}, [length(test{i})/17 17]);
 end
@@ -31,17 +34,17 @@ if cardToGetEncoder == 0
     encoderInd = 1;
 else
     encoderInd = find(cardSN == cardToGetEncoder, 1);
-    [s,v] = getCounterFromEncoder(test{cardToGetEncoder}(:,...
-        encoderChannels(cardToGetEncoder)),freq,lenPerSector);
+    [s,v] = getCounterFromEncoder(test{encoderInd}(:,...
+        encoderChannels(encoderInd)),freq,lenPerSector);
 end
 
 %% Sync cards
-% please provide some important info here
-encoderAvailable = 1;
-encoderChannel = encoderChannels(1);
-encoderChannel2 = encoderChannels(2);
-% parameters regarding synchronization; no need to change
-encoderShift = 3;
+% parameters regarding synchronization; no need to change unless sync is
+% failed; if failed, observe the encoder pattern: if the encoder is sparse
+% then increase encoderSpacingThrsh and decrease otherwise, and if there is
+% a warning saying matrix exceeds dimension then decrease
+% encoderXcorrWindow.
+encoderShift = 3;    % encoder is usually 0/4.6 volts, so pick in between
 encoderSpacingThrsh = 5e2;
 encoderXcorrWindow = 1e5;
 
@@ -53,11 +56,12 @@ if ~isempty(filename)
     encoderjump = 1;
     spacing = 0;
     data1 = test{1};
-    encoderjumpavg = zeros(1,length(filename) - 1);
+    encoderjumpavg = zeros(1,length(test) - 1);
     if encoderAvailable == 1
+        encoderCh1 = encoderChannel(1);
         for j = 2:length(data1)
-            if (data1(j-1,encoderChannel) - encoderShift)*...
-                    (data1(j,encoderChannel) - encoderShift) < 0
+            if (data1(j-1,encoderCh1) - encoderShift)*...
+                    (data1(j,encoderCh1) - encoderShift) < 0
                 encoderbefore = encodercurrent;
                 encodercurrent = j;
                 spacing = encodercurrent - encoderbefore;
@@ -67,21 +71,20 @@ if ~isempty(filename)
                 end
             end
         end
-        s1 = data1(encoderjump:encoderjump + encoderXcorrWindow,encoderChannel);
+        s1 = data1(encoderjump:encoderjump + encoderXcorrWindow,encoderCh1);
         
-        for i=2:length(filename)
-            s2 = test{i}(encoderjump: encoderjump + encoderXcorrWindow,encoderChannel2);
+        for i=2:length(test)
+            s2 = test{i}(encoderjump: encoderjump + encoderXcorrWindow,encoderChannel(i));
             [r,lag] = xcorr(s1,s2);
             [~,I] = max(abs(r));
             encoderjumpavg(i-1) = round(-lag(I));
         end
     end
     % create time col vector
-    timecell = cell(1,length(filename));
-    rawTcell{1} = 1/freq * (1:length(data1))';
-    timecell{1} = rawTcell{1};
+    timecell = cell(1,length(test));
+    timecell{1} = 1/freq * (1:length(data1))';
     
-    for i=2:length(filename)
+    for i=2:length(test)
         timecell{i} = ...
             timecell{1}-encoderjumpavg(i-1)/freq; % apply time shift
     end
@@ -89,11 +92,9 @@ if ~isempty(filename)
 end
 
 %% Plotting
-% can display data median-filtered, smoothened and demeaned; there is no
-% scaling for strain gage data; there is scaling for AE data; no
-% smoothening for AE; smoothening for strain gage data; after importing the
-% data file, simply change parameters and run this section with 'Run and
-% Advance' button;
+% can display data median-filtered, smoothened and demeaned, as well as
+% scaled. After the above sections are run, just simply run the section to
+% plot different versions of data plots catered to your needs.
 
 % parameters to change before executing the following code
 cardToShow = [1,2];    % cards to display
@@ -104,26 +105,31 @@ chSN = [16,13,10,7,4,1;16,13,10,7,4,1]; % third gage (shear)
 % chSN = [16,14,11,8,5,2;16,14,11,8,5,2];    % normal load channels to display on each card
 % chSN = [2,5,8,11,14;2,5,8,11,14];    % channels to display on each card
 % chSN = [1,3,4,6,7,9,10,12,13,15;1,3,4,6,7,9,10,12,13,15];    % channels to display on each card
-smoothSpan = 1;    % smoothening window; make it 1 to diable smoothening; note there is no smoothening for AE data
+smoothSpan = [1,1];    % smoothening window; make it 1 to diable smoothening; note there is no smoothening for AE data
+medFilterOn = [1,1];    % choosing 1 makes median filter on; median filter gets rid of spikes
 cardOffset = 0.2;    % offset between cards when plotting
 chOffset = 0.03;    % offset between channels within each card when plotting
+cardAmp = [1,10];
+encoderVelDis = 1;    % velocity & distance from encoder; 0 for not plotting; 1 for only velocity; 2 for v & d
 sSlope = 1e3;    % slope of counter for scaling when plotting
 vSlope = 1e3;    % slope of velocity for scaling when plotting
-encoderSlope = 0.01;
-ACQ1001Amp = 10;
-accelCh = 16;
+encoderSlope = [0.01,0.01];
+accelCard = 2;    % only support one card
+accelCh = 16;    % only support one channel
 accelAmp = 1;
-encoderVelDis = 2;    % a constant; don't change
-medFilterOn = 1;    % choosing 1 makes median filter on; median filter gets rid of spikes
 
 % execution begins here
-figure('Name',[filename{1},' ',filename{2}]);
+figureTitle = '';
+for i = 1:length(filename)
+    figureTitle = [figureTitle,' ',filename{i}];
+end
+figure('Name',figureTitle);
 hold on;
 
 for j = 1:length(cardToShow)
     tempInd = find(cardSN == cardToShow(j),1);
     for i = 1:length(chSN(j,:))
-        if medFilterOn
+        if medFilterOn(cardToShow(j))
             tempCh = medfilt2(test{tempInd}(:,chSN(j,i)),[3,1]);
         else
             tempCh = test{tempInd}(:,chSN(j,i));
@@ -170,6 +176,6 @@ legend (strings);
 
 xlabel ('Time in seconds');
 ylabel (['Velocity * ',num2str(1/vSlope,'%.1e'),...
-    ' m/s',sprintf('\n'),'Distance * ',num2str(1/sSlope,'%.1e'),' m']);
+    ' m/s',newline,'Distance * ',num2str(1/sSlope,'%.1e'),' m']);
 
 hold off;
